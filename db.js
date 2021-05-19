673bd87b1af599493c7f6769b0049662e62e57e9
Tatsiana Novik <Tatsiana_Novik@epam.com>
2021-05-19 16:17:08 +0300

    fix: add eslint and correct errors

--- db.js
+++ db.js
@@ -2,16 +2,16 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-})
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+});
 sequelize.authenticate().then(
-    function success() {
-        console.log("Connected to DB");
-    },
+  () => {
+    console.log('Connected to DB');
+  },
 
-    function fail(err) {
-        console.log(`Error: ${err}`);
-    }
-)
+  (err) => {
+    console.log(`Error: ${err}`);
+  },
+);
 module.exports = sequelize;
